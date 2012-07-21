5deb969a8d1c81d81df896527850fae1be5f8114
Arthur Puyou <arthur@puyou.fr>
2012-07-21 16:05:56 +0200

    Nettoyage code codeurs

--- encoder.cpp
+++ encoder.cpp
@@ -7,13 +7,6 @@
 #include "encoder.h"
 #include "include_arduino.h"
 
-
-
-/*int state_left_pinA;
-int state_left_pinB;
-int state_right_pinA;
-int state_right_pinB;*/
-
 long value_left_enc;
 long value_right_enc;
 
@@ -31,91 +24,21 @@ void initEncoders(){
 	digitalWrite(PIN_RIGHT_A, HIGH);
 	digitalWrite(PIN_RIGHT_B, HIGH);
 
-/*	state_left_pinA = digitalRead(PIN_LEFT_A);
-	state_left_pinB = digitalRead(PIN_LEFT_B);
-	state_right_pinA = digitalRead(PIN_RIGHT_A);
-	state_right_pinB = digitalRead(PIN_RIGHT_B);*/
-
 	attachInterrupt(INTERRUPT_LEFT_A,valueChangeOnEncoderLeftPinA,RISING);
-	//attachInterrupt(INTERRUPT_LEFT_B,valueChangeOnEncoderLeftPinB,CHANGE);
 	attachInterrupt(INTERRUPT_RIGHT_A,valueChangeOnEncoderRightPinA,RISING);
-	//attachInterrupt(INTERRUPT_RIGHT_B,valueChangeOnEncoderRightPinB,CHANGE);
 }
 
 
 void valueChangeOnEncoderLeftPinA(){
-	//int new_state = VALUE_PIN_LEFT_A;
-	//int new_state = digitalRead(PIN_LEFT_A);
-
-	//if(new_state == HIGH)
-		if(VALUE_PIN_LEFT_B == HIGH)
-			value_left_enc--;
-		else
-			value_left_enc++;
-
-	/*else
-		if(state_left_pinB == HIGH)
-			value_left_enc++;
-		else
-			value_left_enc--;*/
-
-	//state_left_pinA = new_state;
+	if(VALUE_PIN_LEFT_B == HIGH)
+      		value_left_enc--;
+      	else
+      		value_left_enc++;
 }
-
-/*void valueChangeOnEncoderLeftPinB(){
-	int new_state = VALUE_PIN_LEFT_B;
-	//int new_state = digitalRead(PIN_LEFT_B);
-
-	if(new_state == HIGH)
-		if(state_left_pinA == HIGH)
-			value_left_enc++;
-		else
-			value_left_enc--;
-
-	else
-		if(state_left_pinA == HIGH)
-			value_left_enc--;
-		else
-			value_left_enc++;
-
-	state_left_pinB = new_state;
-}*/
 
 void valueChangeOnEncoderRightPinA(){
-	//int new_state = VALUE_PIN_RIGHT_A;
-	//int new_state = digitalRead(PIN_RIGHT_A);
-
-	//if(new_state == HIGH)
-		if(VALUE_PIN_RIGHT_B == HIGH)
-			value_right_enc--;
-		else
-			value_right_enc++;
-
-	/*else
-		if(state_right_pinB == HIGH)
-			value_right_enc++;
-		else
-			value_right_enc--;*/
-
-	//state_right_pinA = new_state;
-}
-
-/*void valueChangeOnEncoderRightPinB(){
-	int new_state = VALUE_PIN_RIGHT_B;
-	//int new_state = digitalRead(PIN_RIGHT_B);
-
-	if(new_state == HIGH)
-		if(state_right_pinA == HIGH)
-			value_right_enc++;
-		else
-			value_right_enc--;
-
+	if(VALUE_PIN_RIGHT_B == HIGH)
+		value_right_enc--;
 	else
-		if(state_right_pinA == HIGH)
-			value_right_enc--;
-		else
-			value_right_enc++;
-
-	state_right_pinB = new_state;
-}*/
-
+		value_right_enc++;
+}
